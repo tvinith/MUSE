434d446a21c5681f3623cba3a036cb272ea62dac
OpsWorks user vinithkumart <vinithkumart@ds-gpu1-10189.localdomain>
2019-11-06 13:23:51 +0000

    init commit

---
+++ example_supervised.sh
@@ -0,0 +1,7 @@
+python unsupervised.py   --src_lang en --tgt_lang es --src_emb data/vect/wiki.en.vec --tgt_emb data/vect/wiki.es.vec \
+		         --cuda True \
+			 --exp_name "en_es" \
+	            	 --exp_id "002" \
+			 --n_refinement 0 \
+			 --n_epochs 1
+
