c39033c7a9feda5ae384ec1a3d15afd35566fcec
tvinith <tvinith@github.com>
2019-11-06 20:41:09 +0530

    added dico build S2T&T2S

--- example_supervised.sh
+++ example_supervised.sh
@@ -1,7 +1,7 @@
 python unsupervised.py   --src_lang en --tgt_lang es --src_emb data/vect/wiki.en.vec --tgt_emb data/vect/wiki.es.vec \
 		         --cuda True \
+						 --dico_build "S2T&T2S" \
 			 --exp_name "en_es" \
 	            	 --exp_id "002" \
 			 --n_refinement 0 \
 			 --n_epochs 1
-
