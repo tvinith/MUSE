e79b4c7054d06075ad600b78807a282d43a7e02e
tvinith <tvinith@github.com>
2019-11-07 12:16:31 +0530

    -initial read vect

---
+++ src/read_vect.py
@@ -0,0 +1,80 @@
+import numpy as np
+import os
+import sys
+import argparse
+import torch
+
+
+parser = argparse.ArgumentParser(description='GRET training')
+parser.add_argument("--src_lang", type=str, default="", help="source language")
+parser.add_argument("--tgt_lang", type=str, default="", help="target language")
+parser.add_argument("--src_path", type=str, default="", help="source embeddings path")
+parser.add_argument("--tgt_path", type=str, default="", help="target embeddigns path")
+parser.add_argument("--dico_path", type=str, default="", help="dictionary path")
+
+params = parser.parse_args()
+
+
+
+def load_vec(emb_path):
+    vectors = []
+    word2id = {}
+    with io.open(emb_path, 'r', encoding='utf-8', newline='\n', errors='ignore') as f:
+        next(f)
+        for i, line in enumerate(f):
+            word, vect = line.rstrip().split(' ', 1)
+            vect = np.fromstring(vect, sep=' ')
+            assert word not in word2id, 'word found twice'
+            vectors.append(vect)
+            word2id[word] = len(word2id)
+
+    id2word = {v: k for k, v in word2id.items()}
+    embeddings = np.vstack(vectors)
+    return embeddings, id2word, word2id
+
+def load_dictionary(path, word2id1, word2id2,src_embed,tgt_embed):
+    """
+    Return a torch tensor of size (n, 2) where n is the size of the
+    loader dictionary, and sort it by source word frequency.
+    """
+    assert os.path.isfile(path)
+
+    pairs = []
+    not_found = 0
+    not_found1 = 0
+    not_found2 = 0
+
+    with io.open(path, 'r', encoding='utf-8') as f:
+        for _, line in enumerate(f):
+            assert line == line.lower()
+            word1, word2 = line.rstrip().split()
+            if word1 in word2id1 and word2 in word2id2:
+                pairs.append((word1, word2))
+            else:
+                not_found += 1
+                not_found1 += int(word1 not in word2id1)
+                not_found2 += int(word2 not in word2id2)
+
+    logger.info("Found %i pairs of words in the dictionary (%i unique). "
+                "%i other pairs contained at least one unknown word "
+                "(%i in lang1, %i in lang2)"
+                % (len(pairs), len(set([x for x, _ in pairs])),
+                   not_found, not_found1, not_found2))
+
+    # sort the dictionary by source word frequencies
+    pairs = sorted(pairs, key=lambda x: word2id1[x[0]])
+    dico = torch.LongTensor(len(pairs), 2)
+    for i, (word1, word2) in enumerate(pairs):
+        dico[i, 0] = src_embed[word2id1[word1]]
+        dico[i, 1] = tgt_embed[word2id2[word2]]
+    return dico
+
+
+def main():
+    src_embed,id2word1, word2id1 = load_vec(params.src_path)
+    tgt_embed,id2word2, word2id2 = load_vec(params.tgt_path)
+                        dico = load_dictionary(params.dico_path,word2id1=,word2id2,src_embed,tgt_embed)
+
+
+if __name__ = "__main__":
+    main
