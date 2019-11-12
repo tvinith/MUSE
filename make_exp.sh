8c742df6024dc7e13e5eda220ed10cdda39547f6
tvinith <tvinith@github.com>
2019-11-12 10:03:18 +0530

    make exp check16

--- make_exp.sh
+++ make_exp.sh
@@ -6,6 +6,9 @@ vect_src='en'
 vect_tgt='es'
 vect_src_path='data/vect/wiki.'+$vect_src+'.vec'
 vect_tgt_path='data/vect/wiki.'+$vect_tgt+'.vec'
+echo "Vect Source: $vect_src_path"
+echo "Vect Target: $vect_tgt_path"
+
 bash run_exp.sh UNSUP $vect_src $vect_tgt \
                       $vect_src_path \
                        $vect_tgt_path \
