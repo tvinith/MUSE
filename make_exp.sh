6743a2d8173bfd25ef38e96146d28dbb16d8310d
tvinith <tvinith@github.com>
2019-11-12 09:54:09 +0530

    make exp check12

--- make_exp.sh
+++ make_exp.sh
@@ -6,7 +6,7 @@ vect_src='en'
 vect_tgt='es'
 vect_src_path = 'data/vect/wiki.'+${vect_src}+'.vec'
 vect_tgt_path = 'data/vect/wiki.'+${vect_tgt}+'.vec'
-bash run_exp.sh UNSUP ${vect_src} ${vect_tgt} \
-                      ${vect_src_path} \
-                       ${vect_tgt_path} \
+bash run_exp.sh UNSUP $vect_src $vect_tgt \
+                      $vect_src_path \
+                       $vect_tgt_path \
                       $RANDOM
