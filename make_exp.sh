2cb535d85d13a292ea14e34b66566a0f4a56da93
tvinith <tvinith@github.com>
2019-11-16 22:59:47 +0530

    added bash script for exp

--- make_exp.sh
+++ make_exp.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
 
 
+function runExperiment {
+  #statements
 
-vect_src='en'
-vect_tgt='es'
-vect_src_path='data/vect/wiki.'$vect_src'.vec'
-vect_tgt_path='data/vect/wiki.'$vect_tgt'.vec'
-echo "Vect Source: $vect_src_path"
-echo "Vect Target: $vect_tgt_path"
+  vect_src=$1
+  vect_tgt=$2
+  vect_src_path='data/vect/wiki.'$vect_src'.vec'
+  vect_tgt_path='data/vect/wiki.'$vect_tgt'.vec'
+  echo "Vect Source: $vect_src_path"
+  echo "Vect Target: $vect_tgt_path"
 
-bash run_exp.sh UNSUP $vect_src $vect_tgt \
-                      $vect_src_path \
-                       $vect_tgt_path \
-                      $RANDOM
+  bash run_exp.sh UNSUP $vect_src $vect_tgt \
+                        $vect_src_path \
+                        $vect_tgt_path \
+                        $RANDOM
+}
+
+declare -a lang
+lang=('en' 'es' 'it' 'fr' 'de')
+for i in {0..5}
+  do
+  for j in {1..4}
+    do
+    if [ $i -lt $j ]
+      then
+        runExperiment ${lang[$i]} ${lang[$j]}
+      fi
+    done
+  done
