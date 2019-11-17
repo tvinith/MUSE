67dd169f7eae63983d94891ef18d1446df2f2663
Ubuntu <ubuntu@ip-172-30-3-120.ec2.internal>
2019-11-17 04:01:28 +0000

    make_exp.sh

--- make_exp.sh
+++ make_exp.sh
@@ -2,14 +2,33 @@
 
 
 
-vect_src='en'
-vect_tgt='es'
-vect_src_path='data/vect/wiki.'$vect_src'.vec'
-vect_tgt_path='data/vect/wiki.'$vect_tgt'.vec'
-echo "Vect Source: $vect_src_path"
-echo "Vect Target: $vect_tgt_path"
+function runExperiment {
+  #statements
 
-bash run_exp.sh UNSUP $vect_src $vect_tgt \
-                      $vect_src_path \
-                       $vect_tgt_path \
-                      $RANDOM
+  vect_src=$1
+  vect_tgt=$2
+  vect_src_path='data/vect/wiki.'$vect_src'.vec'
+  vect_tgt_path='data/vect/wiki.'$vect_tgt'.vec'
+  echo "Vect Source: $vect_src_path"
+  echo "Vect Target: $vect_tgt_path"
+
+  bash run_exp.sh UNSUP $vect_src $vect_tgt \
+                        $vect_src_path \
+                        $vect_tgt_path \
+                        $RANDOM
+}
+
+declare -a lang
+lang=('en' 'es' 'it' 'fr' 'de')
+for i in {0..5}
+  do
+  for j in {1..4}
+    do
+    if [ $i -lt $j ]
+      then
+        runExperiment ${lang[$i]} ${lang[$j]}
+      fi
+    done
+  done
+
+
