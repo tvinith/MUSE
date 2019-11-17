f4d67e23618ebf9e77923806b4b9880780466996
tvinith <tvinith@github.com>
2019-11-17 20:21:55 +0530

    make exp

--- make_exp.sh
+++ make_exp.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-
 function runExperiment {
   #statements
 
-<<<<<<< HEAD
-=======
-function runExperiment {
-  #statements
-
->>>>>>> 67dd169f7eae63983d94891ef18d1446df2f2663
   vect_src=$1
   vect_tgt=$2
   vect_src_path='data/vect/wiki.'$vect_src'.vec'
@@ -35,8 +28,3 @@ for i in {0..5}
       fi
     done
   done
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 67dd169f7eae63983d94891ef18d1446df2f2663
